8f22d492c6d2e9363fd732d536663abd39e2a641
Alexander-Ramirez1 <133604422+Alexander-Ramirez1@users.noreply.github.com>
2023-05-26 20:55:43 -0300

    Add files via upload

---
+++ Tp JS/tpjs.js
@@ -0,0 +1,20 @@
+var formularios = document.getElementsByTagName('form');
+
+var ticket=200;
+var cantidad=0;
+
+cantidad=parseInt();
+
+var cantidad = document.getElementById("cantidad").value;
+
+document.getElementById("categoria");
+
+if (categoria == "1"){
+    console.log((ticket*80)/100);
+} else if (categoria == "2"){
+    console.log((ticket*50)/100);
+} else if (categoria == "3"){
+    console.log((ticket*15)/100);
+}
+
+document.getElementById("pago").innerHTML = resultado;
